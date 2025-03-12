fc4ffd7188e096ab46622d5c35ea529c50b55f61
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-12 11:20:31 +0600

    cash out money

--- js/home.js
+++ js/home.js
@@ -3,5 +3,32 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     event.preventDefault();
     const addMoney = document.getElementById('input-add-money').value;
     const pinNumber = document.getElementById('input-pin-number').value;
-    console.log(addMoney, pinNumber);
+    if(pinNumber === '1234'){
+        const balance = document.getElementById('current-balance').innerText;
+        // new balance
+        const newBalance = parseFloat(balance) + parseFloat(addMoney);
+        document.getElementById('current-balance').innerText = newBalance;
+        console.log(newBalance);
+    }else{
+        alert('failed to add money');
+    }
+    // console.log(addMoney, pinNumber);
+});
+
+
+// cash out
+document.getElementById('btn-cashout').addEventListener('click', function(event){
+    event.preventDefault();
+    const addMoney = document.getElementById('input-cashout').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    if(pinNumber === '1234'){
+        const balance = document.getElementById('current-balance').innerText;
+        // new balance
+        const newBalance = parseFloat(balance) - parseFloat(addMoney);
+        document.getElementById('current-balance').innerText = newBalance;
+        console.log(newBalance);
+    }else{
+        alert('failed to add money');
+    }
+    // console.log(addMoney, pinNumber);
 });
