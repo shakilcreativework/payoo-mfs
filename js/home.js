add979e667813ca77ae40925c88169cedc9359da
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-12 10:36:44 +0600

    add money procsess

--- js/home.js
+++ js/home.js
@@ -1,1 +1,7 @@
-console.log('home added');
+// add money to the account
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    const addMoney = document.getElementById('input-add-money').value;
+    const pinNumber = document.getElementById('input-pin-number').value;
+    console.log(addMoney, pinNumber);
+});
